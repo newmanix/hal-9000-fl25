1d678248659d4c95cbaf8e5aea8d60962657dd80
Bill Newman <william.newman@seattlecolleges.edu>
2025-11-20 03:30:30 +0000

    Adds Hal program

---
+++ hal-9000.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#A script to demo  user input and variable use
+
+echo "Hello.  I'm HAL 9000. What is your name?"
+
+read USERNAME
+
+echo "I'm sorry $USERNAME.  I'm afraid I can't open the pod bay doors"
